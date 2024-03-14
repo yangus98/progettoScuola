7395e2a21ec604bcf30d8eb9c626a316bdd953e2
Pasquale De Mauro <demaurolg@gmail.com>
2024-03-14 15:45:52 +0100

    ProvaJs

--- JS/app.js
+++ JS/app.js
@@ -1,30 +1,16 @@
-// Seleziona il pulsante
-const nostriCorsiBtn = document.getElementById("nostri-corsi");
-// Seleziona la tendina del menu
-const divContainer = document.getElementById("tendina-menù");
+const divContainer = document.querySelector("#tendina-menù");
 
-// Aggiungi un gestore di eventi per il clic sul pulsante
-nostriCorsiBtn.addEventListener("click", function() {
-    // Controlla se la tendina del menu è visibile o nascosta
-    if (divContainer.style.visibility === "visible") {
-        // Se è visibile, nascondila
-        divContainer.style.visibility = "hidden";
-    } else {
-        // Altrimenti, mostrala
-        divContainer.style.visibility = "visible";
-    }
-});
+let isClicked = true;
 
-nostriCorsiBtn.addEventListener("mouseover", function() {
-    // Controlla se la tendina del menu è visibile o nascosta
-    if (divContainer.style.visibility === "visible") {
-        // Se è visibile, nascondila
-        divContainer.style.visibility = "hidden";
+let showOrHide = function(){
+    if (isClicked){
+    divContainer.style.visibility = "visible";
+    isClicked = false;
     } else {
-        // Altrimenti, mostrala
-        divContainer.style.visibility = "visible";
+    divContainer.style.visibility = "hidden";
+    isClicked = true;
     }
-});
+}
 
 
 
